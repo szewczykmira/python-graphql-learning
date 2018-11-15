5315796bb3437a626de92594f285fe8568e313ae
Mira Szewczyk <szewczyk.mira@gmail.com>
2018-11-15 14:23:06 +0100

    Add mutation schema

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -9,6 +9,25 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+        return CreateLink(id=link.id, url=link.url, description=link.description)
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
+
+
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
